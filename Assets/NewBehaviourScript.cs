c09378b8c8e18580881dfa1a47a049da610b53c6
Andrew Allbright <aallbrig@gmail.com>
2020-09-02 15:26:45 -0400

    Custom scripts created

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int foobar;
+    void Awake()
+    {
+        Debug.Log("Awake");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update!");
+    }
+}
